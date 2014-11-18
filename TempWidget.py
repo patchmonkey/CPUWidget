9a1b7764b03003b9b1fe799ca2c68520655e8177
th3j3ster <chris.carducci@gmail.com>
2014-11-18 17:44:05 -0500

    Update TempWidget.py

--- TempWidget.py
+++ TempWidget.py
@@ -2,6 +2,8 @@
 
 import sys
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import QSizePolicy as qs
+
 import wmi
 import os
 
@@ -20,6 +22,10 @@ class Example(QtGui.QWidget):
     @QtCore.pyqtSlot() # This is how you set up a custom slot!!!
     def updateTick(self):
         self.temp_CPU.setText("<html><head/><body><p align=\"center\">" + str(getTemp(4)) + "</p></body></html>")
+        self.temp_core1.setText("<html><head/><body><p align=\"center\">" + str(getTemp(0)) + "</p></body></html>")
+        self.temp_core2.setText("<html><head/><body><p align=\"center\">" + str(getTemp(1)) + "</p></body></html>")
+        self.temp_core3.setText("<html><head/><body><p align=\"center\">" + str(getTemp(2)) + "</p></body></html>")
+        self.temp_core4.setText("<html><head/><body><p align=\"center\">" + str(getTemp(3)) + "</p></body></html>")
         print "Updating..."
         
 
@@ -44,113 +50,139 @@ class Example(QtGui.QWidget):
         if e.key() == QtCore.Qt.Key_Escape:
             self.close()
 
-
-
+ 
 ##################################################################################
     def __init__(self):
         super(Example, self).__init__()    #need this to init the parent classd
 
-        self.resize(400, 400)
-        self.setStyleSheet("background-color: rgb(0, 0, 0);")
+        self.resize(400, 200)
+        self.setStyleSheet("background-color: rgb(0,0,0)\n")
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        self.setWindowOpacity(.75)
         self.closeButton = QtGui.QPushButton()
-        self.closeButton.setGeometry(QtCore.QRect(160, 10, 20, 20))
-        #self.closeButton.setStyleSheet("background-color: rgba(0, 0, 0, 10);\n"
-
-#"selection-color: rgb(255, 255, 255);\n"
-#"border-color: rgb(0, 0, 255);\n"
-#"color: rgb(244, 0, 0);\n"
-#"font: 75 12pt \"Arial\";")
+        self.closeButton.setText("x")
+        self.closeButton.setGeometry(QtCore.QRect(180, 20, 20, 20))
+        self.closeButton.setStyleSheet("background-color: rgb(255, 0, 0);\n"
+"color: rgb(255, 0, 0);\n"
+"font: 75 12pt \"Arial\";")
         self.closeButton.setObjectName("closeButton")
-        #self.widget = QtGui.QWidget()
-        
-        #self.widget.setObjectName("widget")
+
         self.gridLayout = QtGui.QGridLayout(self)
-        self.gridLayout.setMargin(0)
+        self.gridLayout.setMargin(30)
+
         self.gridLayout.setObjectName("gridLayout")
-        self.gridLayout.setGeometry(QtCore.QRect(100, 10, 101, 151))  ######
+        self.gridLayout.setGeometry(QtCore.QRect(0, 10, 101, 151))  ######
+##        self.gridLayout.setColumnMinimumWidth (0, 40)
+##        self.gridLayout.setColumnMinimumWidth (1, 100)
 
         self.lab_core3 = QtGui.QLabel()
         self.lab_core3.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
-"border-color: rgba(0, 0, 127, 200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
+"border-color: rgba(0, 0, 127, 255);\n"
 "color: rgba(0, 255, 0, 150);\n"
 "font: 87 9pt \"Arial Black\";")
         self.lab_core3.setObjectName("lab_core3")
-        self.gridLayout.addWidget(self.lab_core3, 3, 0, 1, 1)
+        #self.lab_core3.setFixedWidth(100)
+        self.gridLayout.addWidget(self.lab_core3, 3, 1, 1, 1)
+        
         self.lab_core4 = QtGui.QLabel()
+        #self.lab_core4.setFixedWidth(100)
         self.lab_core4.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
 "border-color: rgba(0, 0, 127, 200);\n"
-"color: rgba(0, 255, 0, 150);\n"
+"color: rgba(0, 255, 0, 150);"
 "font: 87 9pt \"Arial Black\";")
         self.lab_core4.setObjectName("lab_core4")
-        self.gridLayout.addWidget(self.lab_core4, 4, 0, 1, 1)
+        self.lab_core4.setSizePolicy(qs.Ignored, qs.Ignored)
+        self.gridLayout.addWidget(self.lab_core4, 4, 1, 1, 1)
         self.lab_core1 = QtGui.QLabel()
+        #self.lab_core1.setFixedWidth(100)
         self.lab_core1.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
 "border-color: rgba(0, 0, 127, 200);\n"
 "color: rgba(0, 255, 0, 150);\n"
 "font: 87 9pt \"Arial Black\";")
         self.lab_core1.setObjectName("lab_core1")
-        self.gridLayout.addWidget(self.lab_core1, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.lab_core1, 1, 1, 1, 1)
         self.lab_core2 = QtGui.QLabel()
+        #self.lab_core2.setFixedWidth(100)
         self.lab_core2.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
 "border-color: rgba(0, 0, 127, 200);\n"
 "color: rgba(0, 255, 0, 150);\n"
 "font: 87 9pt \"Arial Black\";")
         self.lab_core2.setObjectName("lab_core2")
-        self.gridLayout.addWidget(self.lab_core2, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.lab_core2, 2, 1, 1, 1)
         self.lab_CPU = QtGui.QLabel()
+        #self.lab_CPU.setFixedWidth(100)
         self.lab_CPU.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"        
 "border-color: rgba(0, 0, 127, 200);\n"
 "color: rgba(0, 255, 0, 150);\n"
 "font: 87 9pt \"Arial Black\";")
         self.lab_CPU.setObjectName("lab_CPU")
         self.gridLayout.addWidget(self.lab_CPU, 0, 1, 1, 1)
-        #self.widget1 = QtGui.QWidget()
-        #
-        #self.widget1.setObjectName("widget1")
-        self.gridLayout = QtGui.QGridLayout(self)
-        self.gridLayout.setGeometry(QtCore.QRect(10, 10, 48, 151))
-        self.gridLayout.setMargin(0)
-       # self.gridLayout.setObjectName("gridLayout")
+
         self.temp_CPU = QtGui.QLabel()
+        #self.temp_CPU.setFixedWidth(40)
         self.temp_CPU.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
 "border-color: rgba(0, 0, 127, 200);\n"
 "color: rgba(255, 0, 0, 150);\n"
 "font: 75 14pt \"Arial\";\n"
 "")
         self.temp_CPU.setObjectName("temp_CPU")
-        self.gridLayout.addWidget(self.temp_CPU, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.temp_CPU, 0, 0, 1, 1)
         self.temp_core1 = QtGui.QLabel()
         self.temp_core1.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
 "border-color: rgba(0, 0, 127, 200);\n"
 "color: rgba(255, 0, 0, 150);\n"
 "font: 75 14pt \"Arial\";\n"
 "")
         self.temp_core1.setObjectName("temp_core1")
-        self.gridLayout.addWidget(self.temp_core1, 1, 1, 1, 1)
+        #self.temp_core1.setFixedWidth(40)
+        
+        self.gridLayout.addWidget(self.temp_core1, 1, 0, 1, 1)
         self.temp_core2 = QtGui.QLabel()
         self.temp_core2.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
 "border-color: rgba(0, 0, 127, 200);\n"
 "color: rgba(255, 0, 0, 150);\n"
 "font: 75 14pt \"Arial\";\n"
 "")
         self.temp_core2.setObjectName("temp_core2")
-        self.gridLayout.addWidget(self.temp_core2, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.temp_core2, 2, 0, 1, 1)
         self.temp_core3 = QtGui.QLabel()
+        #self.temp_core1.setFixedWidth(40)
         self.temp_core3.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
 "border-color: rgba(0, 0, 127, 200);\n"
 "color: rgba(255, 0, 0, 150);\n"
 "font: 75 14pt \"Arial\";\n"
 "")
         self.temp_core3.setObjectName("temp_core3")
-        self.gridLayout.addWidget(self.temp_core3, 3, 1, 1, 1)
+        self.gridLayout.addWidget(self.temp_core3, 3, 0, 1, 1)
         self.temp_core4 = QtGui.QLabel()
+        #self.temp_core1.setFixedWidth(40)
         self.temp_core4.setStyleSheet("background-color: rgba(0, 0, 0,200);\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
 "border-color: rgba(0, 0, 127, 200);\n"
 "color: rgba(255, 0, 0, 150);\n"
 "font: 75 14pt \"Arial\";\n"
 "")
         self.temp_core4.setObjectName("temp_core4")
-        self.gridLayout.addWidget(self.temp_core4, 4, 1, 1, 1)
+        self.gridLayout.addWidget(self.temp_core4, 4, 0, 1, 1)
         self.setWindowTitle("Temp Gauge")
 				
         self.closeButton.setText("x")
@@ -159,39 +191,14 @@ class Example(QtGui.QWidget):
         self.lab_core1.setText("Core 1 Temp")
         self.lab_core2.setText("Core 2 Temp")
         self.lab_CPU.setText("CPU Temp")
-        self.temp_CPU.setText("<html><head/><body><p align=\"center\">4</p></body></html>")
-        self.temp_core1.setText("<html><head/><body><p align=\"center\">0</p></body></html>")
-        self.temp_core2.setText("<html><head/><body><p align=\"center\">1</p></body></html>")
-        self.temp_core3.setText("<html><head/><body><p align=\"center\">2</p></body></html>")
-        self.temp_core4.setText("<html><head/><body><p align=\"center\">3</p></body></html>")
+        self.temp_CPU.setText("<html><head/><body><p align=\"center\">" + str(getTemp(4)) + "</p></body></html>")
+        self.temp_core1.setText("<html><head/><body><p align=\"center\">" + str(getTemp(0)) + "</p></body></html>")
+        self.temp_core2.setText("<html><head/><body><p align=\"center\">" + str(getTemp(1)) + "</p></body></html>")
+        self.temp_core3.setText("<html><head/><body><p align=\"center\">" + str(getTemp(2)) + "</p></body></html>")
+        self.temp_core4.setText("<html><head/><body><p align=\"center\">" + str(getTemp(3)) + "</p></body></html>")
 		
 
-
-
-
-
-
-
-
-
-
-
-##        self.setGeometry(2000, 100, 250, 150)
-##        self.setWindowTitle('Event handler')
-##        self.closeButton = QtGui.QPushButton(self)
-##        self.closeButton.setText("X")
-##        self.closeButton.setGeometry(QtCore.QRect(190, 90, 25, 19))
-##        self.label = QtGui.QLabel(self)
-##        self.label.setText(str(getTemp(4)))
-##        self.label.setGeometry(30,30,30,30)
-
-
-
-
-
-
-
-        #self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.show()
 
 
@@ -225,3 +232,19 @@ if __name__ == '__main__':
 #self.setWindowFlags(QtCore.Qt.FramelessWindowHint) #this hides the frame
 #QtCore.QObject.connect(self.closeButton, QtCore.SIGNAL("clicked()"), )
 #QtCore.QMetaObject.connectSlotsByName(self)
+##        self.setGeometry(2000, 100, 250, 150)
+##        self.setWindowTitle('Event handler')
+##        self.closeButton = QtGui.QPushButton(self)
+##        self.closeButton.setText("X")
+##        self.closeButton.setGeometry(QtCore.QRect(190, 90, 25, 19))
+##        self.label = QtGui.QLabel(self)
+##        self.label.setText(str(getTemp(4)))
+##        self.label.setGeometry(30,30,30,30)
+
+
+        #self.widget1 = QtGui.QWidget()
+        #self.widget1.setObjectName("widget1")
+        #self.gridLayout = QtGui.QGridLayout(self)
+        #self.gridLayout.setGeometry(QtCore.QRect(10, 10, 48, 151))
+        #self.gridLayout.setMargin(0)
+       # self.gridLayout.setObjectName("gridLayout")
